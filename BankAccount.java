bf32febb297428a956c78b3058ebb9405b9d1b5b
FREDRICK ODONDI <fredrickodondi95@gmail.com>
2025-01-21 18:40:53 +0300

    Similar app but this time i used OOP concepts

---
+++ BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private double balance; //variable to hold account balance
+
+    //Constructor to initialize balance
+    public BankAccount(double initialBalance){
+        balance = initialBalance;
+    }
+    //Method to deposit money accepts amount as parameter
+    public void deposit(double amount){
+        if (balance>0){
+            balance += amount;
+        }else {
+            System.out.println("Must be an integer");
+        }
+    }
+    //Method to withdraw
+    public void withdraw(double amount){
+        if (amount>0 && amount<balance){
+            balance -=amount;
+            System.out.println("Withdrawn: "+ amount);
+        }if (amount>balance){
+            System.out.println("Insufficient funds in your account");
+        }
+    }
+    //method to check balance
+    public double checkBalance(){
+        return balance;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner; // Import the Scanner class
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(10000);
+        System.out.println("Welcome to Equity Bank");
+        //display services
+        System.out.println("1. Withdraw");
+        System.out.println("2. Deposit");
+        System.out.println("3. Show my balance");
+        //Create a scanner tool to read input
+        Scanner scanner = new Scanner(System.in);
+        int selection = scanner.nextInt();
+        //display Deposit
+        if (selection==1){
+            //Deposit Money
+            System.out.println("Enter amount to deposit");
+            double amount_withdrawn = scanner.nextDouble();
+            account.withdraw(amount_withdrawn);
+        } else if (selection==2) {
+            //Withdraw Money
+            System.out.println("Enter amount to withdraw");
+            double amount_to_deposit = scanner.nextDouble();
+            account.deposit(amount_to_deposit);
+        // Show balance
+        } else if (selection==3) {
+            System.out.println("Transaction was successfully completed." +
+                    " Current Balance: "+ account.checkBalance());
+        }else {
+            System.out.println("Invalid Choice");
+        }
+    }
+}
+
